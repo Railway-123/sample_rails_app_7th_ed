4251e5fbf05bc5d2bdfa3e6d45845ceab580b6a1
Joni Lahtinen <joni.lahtinen@gmail.com>
2025-02-01 19:23:02 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #lets set secret_key_base from env if wanted
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
